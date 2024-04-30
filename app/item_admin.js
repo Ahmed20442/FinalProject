d29057b99feedfea459a652b4491bdfb33f8a748
Ahmed <aw172759@gmail.com>
2024-05-01 02:24:42 +0300

    wael

--- app/item_admin.js
+++ app/item_admin.js
@@ -2,69 +2,63 @@ import React from 'react';
 import { StyleSheet, Text, View, Image, Dimensions, Pressable, TouchableOpacity } from 'react-native';
 import { router } from "expo-router";
 
-export default function ItemAdmin({ name, price, image, text, onDelete, onEdit }) {
-    return (
-        <View style={styles.item}>
-            <View style={styles.infoContainer}>
-                <Text style={styles.name}>{name}</Text>
-                <Text style={styles.price}>${price}</Text>
+const screenWidth = Dimensions.get('window').width;
 
-                <Text style={styles.name}>{text}</Text>
-            </View>
+export default function ItemAdmin({ name, price, image, onDelete, onEdit }) {
+    return (
+        <View style={[styles.item, { width: screenWidth / 2 - 30 }]}>
             <Pressable onPress={() => router.push(`/pressedItem?name=${name}&price=${price}&image=${image}`)}>
                 <Image source={{ uri: image }} style={styles.image} />
             </Pressable>
-            <TouchableOpacity style={styles.deleteButton} onPress={onDelete}>
-                <Text style={styles.buttonText}>Delete</Text>
-            </TouchableOpacity>
-            <TouchableOpacity style={styles.deleteButton} onPress={onEdit}>
-                <Text style={styles.buttonText}>Edit</Text>
-            </TouchableOpacity>
+            <View style={styles.infoContainer}>
+                <Text numberOfLines={1} style={styles.name}>{name}</Text>
+                <Text style={styles.price}>${price}</Text>
+
+            </View>
+            <View style={{ flexDirection: 'row', justifyContent: 'space-between' }}>
+                <TouchableOpacity style={styles.deleteButton} onPress={onDelete}>
+                    <Text style={styles.buttonText}>Delete</Text>
+                </TouchableOpacity>
+                <TouchableOpacity style={styles.editButton} onPress={onEdit}>
+                    <Text style={styles.buttonText}>Edit</Text>
+                </TouchableOpacity></View>
         </View>
     );
 }
 
-const screenWidth = Dimensions.get('window').width;
 
 const styles = StyleSheet.create({
     item: {
-        backgroundColor: '#fff',
+        backgroundColor: 'lightgray',
         borderRadius: 10,
-        padding: 20,
-        marginVertical: 5,
-        flexDirection: 'row',
-        alignItems: 'center',
-        justifyContent: 'space-between',
-        width: screenWidth - 20,
-        elevation: 3,
+        padding: 5,
     },
     image: {
-        width: 100,
-        height: 100,
+        width: 80,
+        height: 80,
         borderRadius: 10,
+        marginRight: 20,
     },
     infoContainer: {
-        // flex: 1,
+
     },
     name: {
         fontSize: 18,
         fontWeight: 'bold',
         marginBottom: 5,
+
     },
     price: {
         fontSize: 16,
         color: '#888',
-    }, deleteButton: {
-        backgroundColor: '#FF6347',
-        paddingVertical: 5,
-        paddingHorizontal: 10,
-        borderRadius: 5,
-        marginTop: 5,
     },
     buttonText: {
-        color: '#FFFFFF',
-        fontSize: 12,
-        fontWeight: 'bold',
-        textAlign: 'center',
+        color: 'red',
+        fontSize: 16,
+    },
+    editButton: {
+        fontSize: 16,
     },
 });
+
+
