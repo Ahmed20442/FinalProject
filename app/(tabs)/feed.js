ff4bc2645d88b8165cf22000ddc9bc9c0b0e92f6
Hafez <marwanhany2244@gmail.com>
2024-05-07 22:43:18 +0300

    hafez

--- app/(tabs)/feed.js
+++ app/(tabs)/feed.js
@@ -1,3 +1,209 @@
+// import React, { useEffect, useState } from "react";
+// import { StyleSheet, Text, View, SafeAreaView, FlatList, Pressable, TextInput, ActivityIndicator } from "react-native";
+// import AsyncStorage from '@react-native-async-storage/async-storage';
+// import { collection, getDocs } from "firebase/firestore";
+// import { db } from "../../firebase/firebase";
+// import { useLocalSearchParams, router } from "expo-router";
+// import { getAuth, signOut } from "firebase/auth";
+// import Item from "../Item";
+
+// const Feed = () => {
+//   const [data, setData] = useState([]);
+//   const [text, setText] = useState('');
+//   const [originalData, setOriginalData] = useState([]);
+
+//   const searchItems = (searchFor) => {
+//     const filteredData = originalData.filter(item => item.name.toLowerCase().includes(searchFor.toLowerCase()));
+//     setData(filteredData);
+//   };
+
+//   useEffect(() => {
+//     const fetchData = async () => {
+//       try {
+//         const storedData = await AsyncStorage.getItem('productData');
+//         const storedProductCount = storedData ? JSON.parse(storedData).length : 0;
+
+//         const querySnapshot = await getDocs(collection(db, "Products"));
+//         const firestoreProductCount = querySnapshot.size;
+
+//         if (storedProductCount !== firestoreProductCount) {
+//           const fetchedData = querySnapshot.docs.map(doc => doc.data());
+//           setData(fetchedData);
+//           setOriginalData(fetchedData);
+//           await AsyncStorage.setItem('productData', JSON.stringify(fetchedData));
+//         } else {
+//           setData(JSON.parse(storedData));
+//           setOriginalData(JSON.parse(storedData));
+//         }
+//       } catch (error) {
+//         console.error("Error fetching data: ", error);
+//       }
+//     };
+
+//     fetchData();
+//   }, []);
+
+//   const handleSignOut = async () => {
+//     try {
+//       const auth = getAuth();
+//       await signOut(auth);
+//       router.push("/Login");
+//     } catch (error) {
+//       console.error("Error signing out:", error);
+//     }
+//   };
+
+//   const resetData = () => {
+//     setData(originalData);
+//     setText('');
+//   };
+
+//   return (
+//     <View style={styles.container}>
+//       <SafeAreaView style={styles.safeArea}>
+
+//         <View style={styles.searchContainer}>
+//           <TextInput
+//             style={styles.input}
+//             placeholder="Search for products"
+//             onChangeText={(t) => { setText(t); searchItems(t) }}
+//           />
+//           <Pressable
+//             onPress={() => searchItems(text)}
+//             style={({ pressed }) => [
+//               {
+//                 opacity: pressed ? 0.6 : 1,
+//               },
+//               styles.searchButton,
+//             ]}
+//           >
+//             <Text style={styles.searchButtonText}>Search</Text>
+//           </Pressable>
+//           <Pressable
+//             onPress={resetData}
+//             style={({ pressed }) => [
+//               {
+//                 opacity: pressed ? 0.6 : 1,
+//               },
+//               styles.resetButton,
+//             ]}
+//           >
+//             <Text style={styles.resetButtonText}>Reset</Text>
+//           </Pressable>
+//         </View>
+
+//         <FlatList
+//           style={styles.productList}
+//           data={data}
+//           numColumns={2}
+//           renderItem={({ item }) => (
+//             <View style={[styles.productItem, { width: '50%' }]}>
+//               <Item
+//                 name={item.name}
+//                 price={item.price}
+//                 image={item.image}
+//               />
+//             </View>
+//           )}
+//           ListEmptyComponent={<ActivityIndicator size="large" color="#0000ff" />}
+//         />
+//         <Pressable style={styles.signOutButton} onPress={handleSignOut}>
+//           <Text style={styles.signOutButtonText}>Sign Out</Text>
+//         </Pressable>
+//       </SafeAreaView>
+//     </View>
+//   );
+// };
+
+// export default Feed;
+
+// const styles = StyleSheet.create({
+//   container: {
+//     flex: 1,
+//     backgroundColor: '#F8F8F8',
+//   },
+//   safeArea: {
+//     flex: 1,
+//     padding: 20,
+//     alignItems: 'center',
+//   },
+//   searchContainer: {
+//     flexDirection: "row",
+//     alignItems: "center",
+//     marginBottom: 20,
+//     width: '100%',
+//   },
+//   input: {
+//     flex: 1,
+//     height: 40,
+//     backgroundColor: '#FFFFFF',
+//     borderRadius: 25,
+//     paddingHorizontal: 15,
+//     marginRight: 10,
+//     elevation: 2,
+//     shadowColor: '#000',
+//     shadowOffset: { width: 0, height: 2 },
+//     shadowOpacity: 0.23,
+//     shadowRadius: 2.62,
+//   },
+//   searchButton: {
+//     backgroundColor: '#FF4500',
+//     paddingHorizontal: 20,
+//     paddingVertical: 12,
+//     borderRadius: 18,
+//     elevation: 2,
+//     shadowColor: '#000',
+//     shadowOffset: { width: 0, height: 2 },
+//     shadowOpacity: 0.23,
+//     shadowRadius: 2.62,
+//   },
+//   searchButtonText: {
+//     fontSize: 16,
+//     fontWeight: 'bold',
+//     color: '#FFF',
+//   },
+//   resetButton: {
+//     backgroundColor: '#DDDDDD',
+//     paddingHorizontal: 20,
+//     paddingVertical: 12,
+//     borderRadius: 18,
+//     elevation: 2,
+//     shadowColor: '#000',
+//     shadowOffset: { width: 0, height: 2 },
+//     shadowOpacity: 0.23,
+//     shadowRadius: 2.62,
+//   },
+//   resetButtonText: {
+//     fontSize: 16,
+//     fontWeight: 'bold',
+//     color: '#333',
+//   },
+//   productList: {
+//     width: '100%',
+//   },
+//   productItem: {
+//     padding: 10,
+//     marginBottom: 10,
+//   },
+//   signOutButton: {
+//     backgroundColor: "#FF4500",
+//     paddingVertical: 10,
+//     paddingHorizontal: 20,
+//     borderRadius: 18,
+//     marginTop: 20,
+//     alignSelf: 'center',
+//     elevation: 2,
+//     shadowColor: '#000',
+//     shadowOffset: { width: 0, height: 2 },
+//     shadowOpacity: 0.23,
+//     shadowRadius: 2.62,
+//   },
+//   signOutButtonText: {
+//     color: "white",
+//     fontSize: 18,
+//     fontWeight: "bold",
+//   },
+// });
 import React, { useEffect, useState } from "react";
 import { StyleSheet, Text, View, SafeAreaView, FlatList, Pressable, TextInput, ActivityIndicator } from "react-native";
 import AsyncStorage from '@react-native-async-storage/async-storage';
@@ -6,10 +212,9 @@ import { db } from "../../firebase/firebase";
 import { useLocalSearchParams, router } from "expo-router";
 import { getAuth, signOut } from "firebase/auth";
 import Item from "../Item";
-
+import { Ionicons } from '@expo/vector-icons';
 const Feed = () => {
   const [data, setData] = useState([]);
-  const [text, setText] = useState('');
   const [originalData, setOriginalData] = useState([]);
 
   const searchItems = (searchFor) => {
@@ -55,7 +260,6 @@ const Feed = () => {
 
   const resetData = () => {
     setData(originalData);
-    setText('');
   };
 
   return (
@@ -66,30 +270,9 @@ const Feed = () => {
           <TextInput
             style={styles.input}
             placeholder="Search for products"
-            onChangeText={(t) => { setText(t); searchItems(t) }}
+            onChangeText={(text) => searchItems(text)}
           />
-          <Pressable
-            onPress={() => searchItems(text)}
-            style={({ pressed }) => [
-              {
-                opacity: pressed ? 0.6 : 1,
-              },
-              styles.searchButton,
-            ]}
-          >
-            <Text style={styles.searchButtonText}>Search</Text>
-          </Pressable>
-          <Pressable
-            onPress={resetData}
-            style={({ pressed }) => [
-              {
-                opacity: pressed ? 0.6 : 1,
-              },
-              styles.resetButton,
-            ]}
-          >
-            <Text style={styles.resetButtonText}>Reset</Text>
-          </Pressable>
+           <Ionicons name="search" size={24} color="black" style={styles.searchIcon} />
         </View>
 
         <FlatList
@@ -132,51 +315,27 @@ const styles = StyleSheet.create({
     alignItems: "center",
     marginBottom: 20,
     width: '100%',
+    marginTop:40
   },
   input: {
     flex: 1,
     height: 40,
     backgroundColor: '#FFFFFF',
     borderRadius: 25,
-    paddingHorizontal: 15,
+    paddingHorizontal: 40,
     marginRight: 10,
+    marginLeft:13,
     elevation: 2,
     shadowColor: '#000',
     shadowOffset: { width: 0, height: 2 },
     shadowOpacity: 0.23,
     shadowRadius: 2.62,
   },
-  searchButton: {
-    backgroundColor: '#FF4500',
-    paddingHorizontal: 20,
-    paddingVertical: 12,
-    borderRadius: 18,
-    elevation: 2,
-    shadowColor: '#000',
-    shadowOffset: { width: 0, height: 2 },
-    shadowOpacity: 0.23,
-    shadowRadius: 2.62,
-  },
-  searchButtonText: {
-    fontSize: 16,
-    fontWeight: 'bold',
-    color: '#FFF',
-  },
-  resetButton: {
-    backgroundColor: '#DDDDDD',
-    paddingHorizontal: 20,
-    paddingVertical: 12,
-    borderRadius: 18,
-    elevation: 2,
-    shadowColor: '#000',
-    shadowOffset: { width: 0, height: 2 },
-    shadowOpacity: 0.23,
-    shadowRadius: 2.62,
-  },
-  resetButtonText: {
-    fontSize: 16,
-    fontWeight: 'bold',
-    color: '#333',
+  searchIcon: {
+    position: 'absolute', 
+    left: 15,
+    marginLeft:10, 
+    top: 10, 
   },
   productList: {
     width: '100%',
@@ -204,3 +363,4 @@ const styles = StyleSheet.create({
     fontWeight: "bold",
   },
 });
+
--- app/Item.js
+++ app/Item.js
@@ -1,29 +1,94 @@
+// import React from 'react'; 
+// import { StyleSheet, Text, View, Image, Dimensions, Pressable } from 'react-native'; 
+// import { router } from "expo-router"; 
+ 
+// const screenWidth = Dimensions.get('window').width; 
+ 
+// export default function Item({ name, price, image }) { 
+//   return (
+//     <View style={[styles.item, { width: screenWidth / 2 - 30 }]}>
+//       <Pressable onPress={() => router.push(`/pressedItem?name=${name}&price=${price}&image=${image}`)}>
+//         <Image source={{ uri: image }} style={styles.image} />
+//       </Pressable>
+//       <View style={styles.infoContainer}>
+//         <Text numberOfLines={1} style={styles.name}>{name}</Text>
+//         <Text style={styles.price}>${price}</Text>
+//       </View>
+//     </View>
+//   );
+// }
+  
+ 
+// const styles = StyleSheet.create({ 
+//   item: { 
+//     backgroundColor: 'lightgray',
+//     borderRadius: 10,
+//     padding: 5, 
+//   }, 
+//   image: { 
+//     width: 80,
+//     height: 80,
+//     borderRadius: 10,
+//     marginRight: 20, 
+//   }, 
+//   infoContainer: { 
+  
+//   }, 
+//   name: { 
+//     fontSize: 18,
+//     fontWeight: 'bold',
+//     marginBottom: 5,
+     
+//   }, 
+//   price: { 
+//     fontSize: 16,
+//     color: '#888', 
+//   } 
+// });
 import React from 'react'; 
 import { StyleSheet, Text, View, Image, Dimensions, Pressable } from 'react-native'; 
-import { router } from "expo-router"; 
- 
+import { db } from "./../firebase/firebase"; 
+
 const screenWidth = Dimensions.get('window').width; 
- 
+
 export default function Item({ name, price, image }) { 
+  const addToCart = async () => {
+    try {
+      await db.collection("cartItems").add({
+        name,
+        price,
+        image,
+        quantity: 1
+      });
+      alert('Item added to cart!');
+    } catch (error) {
+      console.error('Error adding item to cart:', error);
+    }
+  };
+
   return (
-    <View style={[styles.item, { width: screenWidth / 2 - 30 }]}>
-      <Pressable onPress={() => router.push(`/pressedItem?name=${name}&price=${price}&image=${image}`)}>
-        <Image source={{ uri: image }} style={styles.image} />
-      </Pressable>
+    <View style={styles.itemContainer}>
+      <Image source={{ uri: image }} style={styles.image} />
       <View style={styles.infoContainer}>
         <Text numberOfLines={1} style={styles.name}>{name}</Text>
         <Text style={styles.price}>${price}</Text>
+        <Pressable onPress={addToCart} style={styles.addToCartButton}>
+          <Text style={styles.addToCartButtonText}>Add to Cart</Text>
+        </Pressable>
       </View>
     </View>
   );
 }
   
- 
 const styles = StyleSheet.create({ 
-  item: { 
+  itemContainer: { 
     backgroundColor: 'lightgray',
     borderRadius: 10,
-    padding: 5, 
+    padding: 5,
+    width: screenWidth / 2 - 30,
+    flexDirection: 'row',
+    alignItems: 'center',
+    marginBottom: 10,
   }, 
   image: { 
     width: 80,
@@ -32,18 +97,29 @@ const styles = StyleSheet.create({
     marginRight: 20, 
   }, 
   infoContainer: { 
-  
+    flex: 1,
   }, 
   name: { 
     fontSize: 18,
     fontWeight: 'bold',
     marginBottom: 5,
-     
   }, 
   price: { 
     fontSize: 16,
     color: '#888', 
-  } 
+    marginBottom: 5,
+  },
+  addToCartButton: {
+    backgroundColor: '#FF4500',
+    paddingVertical: 5,
+    paddingHorizontal: 10,
+    borderRadius: 5,
+  },
+  addToCartButtonText: {
+    color: 'white',
+    fontWeight: 'bold',
+  }
 });
+
 
 
