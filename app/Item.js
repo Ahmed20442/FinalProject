0bbec0d968746b817267bd22131f9d343ce8ca7f
Ahmed <aw172759@gmail.com>
2024-05-12 13:57:49 +0300

    wael

--- app/Item.js
+++ app/Item.js
@@ -1,10 +1,5 @@
-<<<<<<< HEAD
-
-import React, { useEffect, useState } from "react";
-=======
-import React, { useState } from "react";
->>>>>>> b002f05fbb9527d506465a0ab04e634f1ca38a80
 import { StyleSheet, Text, View, Image, Dimensions, Pressable } from 'react-native';
+import React, { useEffect, useState } from "react";
 import { db } from "../firebase/firebase";
 import { addDoc, collection, doc } from '@firebase/firestore';
 import { Ionicons } from '@expo/vector-icons';
